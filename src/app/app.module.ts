4c7e905130bedd34438d4ab776ae1692693cdad2
shmuelS <shmuelshmila0@gmail.com>
2019-01-02 21:16:48 +0200

    add new loan page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { NavigateComponent } from './header/navigate/navigate.component';
+import { LoansComponent } from './loans/loans.component';
+import { NewLoanComponent } from './loans/new-loan/new-loan.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    NavigateComponent
+    NavigateComponent,
+    LoansComponent,
+    NewLoanComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/loans/new-loan/new-loan.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-loan',
+  templateUrl: './new-loan.component.html',
+  styleUrls: ['./new-loan.component.css']
+})
+export class NewLoanComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
